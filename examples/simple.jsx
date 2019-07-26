2e3918a4b312e7264d751c4958944361adb18477
awmleer <awmleer@sparker.xyz>
2019-07-26 14:05:26 +0800

    1 file modified, 2 added

--- examples/simple.jsx
+++ examples/simple.jsx
@@ -1,7 +1,16 @@
 import {memo} from 'react'
 import React from 'react'
 
+export function FooStore() {
+  const [x, setX] = useState(initial)
+  return {
+    x,
+    setX
+  }
+}
+
 export const Simple = memo(function Simple() {
+
   return (
     <div>123</div>
   )
---
+++ examples/simple/simple.jsx
@@ -0,0 +1,17 @@
+import {memo} from 'react'
+import React from 'react'
+
+
+
+
+
+export function Simple() {
+  return (
+    <Provider>123</Provider>
+  )
+}
+
+
+function App() {
+  
+}
